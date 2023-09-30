3d6ec5547ec5a34abf3746f2f167b1ccabc5c6cf
jf908 <josh@thefindons.com>
2023-09-30 12:49:56 +0100

    Add tabs and search to profile

---
+++ sk/src/lib/components/Tabs.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { Tab, TabGroup, TabList } from '@rgossiaux/svelte-headlessui';
+
+  export let value: string | undefined = undefined;
+  export let options: { label: string; value: string; icon?: string; selectedClass?: string }[];
+</script>
+
+<TabGroup on:change={(e) => (value = options[e.detail].value)}>
+  <TabList class="flex-inline p-1 bg-base-muted rounded-lg text-fg-muted">
+    {#each options as option}
+      <Tab
+        class={({ selected }) =>
+          `py-2 px-4 text-sm font-500 rounded ${
+            selected ? ` shadow ${option.selectedClass}` : 'hover:text-fg'
+          }`}
+      >
+        {#if option.icon}
+          <span class="text-xl mr-1 {option.icon}" />
+        {/if}
+        {option.label}
+      </Tab>
+    {/each}
+  </TabList>
+</TabGroup>
--- sk/src/routes/user/[id]/+page.svelte
+++ sk/src/routes/user/[id]/+page.svelte
@@ -2,33 +2,55 @@
   import Avatar from '$lib/components/Avatar.svelte';
   import MovieGrid from '$lib/components/MovieGrid.svelte';
   import Pagination from '$lib/components/Pagination.svelte';
+  import Tabs from '$lib/components/Tabs.svelte';
   import Title from '$lib/components/Title.svelte';
   import { watch } from '$lib/pocketbase';
   import type { MediaResponse, UserMediaResponse } from '$lib/pocketbase/pocketbase-types.js';
 
   export let data;
 
-  function createFilter({ intent, watched }: { intent?: string; watched?: boolean }) {
+  const tabs = [
+    {
+      label: 'Watched',
+      value: 'watched',
+      icon: 'i-ph-clock-clockwise',
+      selectedClass: 'btn-primary',
+    },
+    { label: 'Want', value: 'want', icon: 'i-ph-thumbs-up', selectedClass: 'btn-success' },
+    { label: "Don't want", value: 'dont', icon: 'i-ph-thumbs-down', selectedClass: 'btn-danger' },
+  ] as const;
+  let tab: (typeof tabs)[number]['value'] = 'watched';
+
+  function createFilter(value: typeof tab, query: string) {
     const filter = [`user.id = "${data.user.id}"`];
-    if (intent) {
-      filter.push(`intent = "${intent}"`);
-    }
-    if (watched) {
+    if (value === 'watched') {
       filter.push('watches >= 1');
+    } else {
+      filter.push(`intent = "${value}"`);
     }
+
+    if (query) {
+      filter.push(`media.metadata.title ~ "${query}"`);
+    }
+
     return filter.join(' && ');
   }
 
+  let query = '';
+  $: filter = createFilter(tab, query);
+
   const library = watch<UserMediaResponse<{ media: MediaResponse }>>('user_media', {
-    filter: createFilter({ intent: 'want' }),
+    filter,
     expand: 'media',
   });
+
+  $: library.setFilter(filter);
 </script>
 
 <Title title={data.user.name} />
 
 <section class="relative flex items-end justify-center bg-base-subtle min-h-40">
-  <div class="max-w-xl w-full py-2 flex items-end gap-3">
+  <div class="max-w-3xl w-full py-2 flex items-end gap-3 px-2">
     <div class="mb--6">
       <Avatar class="text-2.5rem" />
     </div>
@@ -36,28 +58,20 @@
   </div>
 </section>
 
+<div
+  class="w-full max-w-3xl flex flex-wrap justify-around md:justify-between items-center gap-2 mt-8 px-2 mb-8 mx-auto"
+>
+  <Tabs options={tabs} bind:value={tab} />
+  <input class="input w-full max-w-16rem" type="text" placeholder="Search..." bind:value={query} />
+</div>
 <div class="p-8">
-  <div class="flex gap-2 justify-center mb-8">
-    <button
-      class="btn btn-primary"
-      on:click={() => library.setFilter(createFilter({ watched: true }))}
-      ><span class="i-ph-clock-clockwise text-xl" /> Watched</button
-    >
-    <button
-      class="btn btn-success"
-      on:click={() => library.setFilter(createFilter({ intent: 'want' }))}
-      ><span class="i-ph-thumbs-up text-xl" /> Want</button
-    >
-    <button
-      class="btn btn-danger"
-      on:click={() => library.setFilter(createFilter({ intent: 'dont' }))}
-      ><span class="i-ph-thumbs-down text-xl" /> Don't want</button
-    >
-  </div>
   <MovieGrid
     movies={$library.items.map((m) => m.expand.media.metadata)}
     intents={$library.items.map((m) => m.intent)}
   />
+  {#if $library.items.length === 0}
+    <p class="text-center">No content.</p>
+  {/if}
   {#if $library.totalPages > 1}
     <div class="flex justify-center mt-4">
       <Pagination {...$library} on:setPage={(e) => library.setPage(e.detail)} />
